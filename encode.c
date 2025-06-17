b4367425d7002d69c96c8f143884b2e33a351245
Netanel Friedman <netanel.friedman6@gmail.com>
2025-06-18 00:33:59 +0300

    Fix encoding

--- encode.c
+++ encode.c
@@ -3,8 +3,7 @@
 
 int main(void) {
     char c;
-    while (!feof(stdin)) {
-        c = fgetc(stdin);
+    while (c = fgetc(stdin), !feof(stdin)) {
         for (uint8_t i = 0; i < 8 * sizeof c; i++)
             fprintf(stdout, ((c >> i) & 1) ? "<one/>" : "<zero/>");
     }
