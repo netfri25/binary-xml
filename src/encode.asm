7b95b6327086cd3b7d7119ec5d41fcbb5c7782e3
Netanel Friedman <netanel.friedman6@gmail.com>
2025-08-28 21:05:41 +0300

    Get rid of the buffers

--- src/encode.asm
+++ src/encode.asm
@@ -2,12 +2,6 @@
 format ELF64 executable
 entry _start
 
-segment readable writeable
-align 64
-popcnt_buffer: db 64 dup 0
-align 64
-byte_buffer: db 64 dup 0
-
 segment readable executable
 _start:
     call init_input
@@ -32,31 +26,34 @@ _start:
 
 .64bytes_loop:
     ; read 64 bytes at once to `byte_buffer`
+    ; this is fine to over-read.
     vmovdqa64 zmm1, [rsi]
-    vmovdqa64 [byte_buffer], zmm1
 
-    ; evaluate the popcnt of each of the bytes and store it in `popcnt_buffer`
-    vpopcntb zmm2, zmm1
-    vmovdqa64 [popcnt_buffer], zmm2
-
-    ; bytes_to_process: r9 = min(64, src_end - src_ptr)
+    ; amount of bytes to process: r9 = min(64, src_end - src_ptr)
+    ; either 64 at once or the leftover bytes if there's less than 64 left
     mov r9, rdx
     sub r9, rsi
     cmp r9, 64
     cmova r9, r8
 
-    ; index
+    ; index inside zmm1
     xor rcx, rcx
 
+    ; TODO: instead of handling one byte try to handle multiple at once
     .byte_loop:
+        vpbroadcastb zmm2, cl    ; zmm2 = [ cl cl .. cl ]
+        vpermb zmm0, zmm2, zmm1  ; zmm0 = [ zmm1[cl] zmm1[cl] .. zmm1[cl] ]
+        vmovd eax, xmm0
+        and rax, 0xFF
+
         ; index in the byte table. since each element in the table is
-        ; 64 bytes, the index should be multiplied by 64, which is 2**6
-        movzx rax, byte [byte_buffer + rcx]
-        shl rax, 6
+        ; 64 bytes, the index should be multiplied by 64, which is 8 * 2**3
+        shl eax, 3
 
         ; copy from the table to the destination
-        vmovdqa64 zmm0, [table + rax]
-        vmovdqu64 [rdi], zmm0
+        vmovdqa64 zmm3, [table + eax*8]
+        vmovdqu64 [rdi], zmm3
+
         ; each length is 64 bit integer, hence indexing is prev mult by 8
         add rdi, qword [lengths + eax]
 
