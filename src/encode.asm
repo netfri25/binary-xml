5bb6ac6cfe11e3fca71f5690d69e7741eeba1a2d
Netanel Friedman <netanel.friedman6@gmail.com>
2025-08-28 18:46:09 +0300

    Implement vectorized popcnt

--- src/encode.asm
+++ src/encode.asm
@@ -2,6 +2,12 @@
 format ELF64 executable
 entry _start
 
+segment readable writeable
+align 64
+popcnt_buffer: db 64 dup 0
+align 64
+byte_buffer: db 64 dup 0
+
 segment readable executable
 _start:
     call init_input
@@ -18,30 +24,59 @@ _start:
 
     mov rcx, [input_len]
 
-.char_loop:
-    movzx rax, byte [rsi]
-    popcnt rbx, rax
+    ; pointer to the end of src
+    lea rdx, [rsi + rcx]
 
-    ; index in the table. since each element in the table is 64 bytes
-    ; the index should be multiplied by 64, which is 2**6
-    shl rax, 6
+    ; can't use constant in cmov lmao
+    mov r8, 64
 
-    ; copy from the table to the destination
-    vmovdqa64 zmm0, [table + rax]
-    vmovdqu64 [rdi], zmm0
+.64bytes_loop:
+    ; read 64 bytes at once to `byte_buffer`
+    vmovdqa64 zmm1, [rsi]
+    vmovdqa64 [byte_buffer], zmm1
 
-    ; the difference between the length of `<zero/>` and the length of `<one/>` is exactly 1 byte,
-    ; so the difference when writing a full byte as "tag bits" is 8 * zero.len - popcnt
-    add rdi, 8 * zero.len
-    sub rdi, rbx
-    inc rsi
+    ; evaluate the popcnt of each of the bytes and store it in `popcnt_buffer`
+    vpopcntb zmm2, zmm1
+    vmovdqa64 [popcnt_buffer], zmm2
 
-    loop .char_loop
+    ; bytes_to_process: r9 = min(64, src_end - src_ptr)
+    mov r9, rdx
+    sub r9, rsi
+    cmp r9, 64
+    cmova r9, r8
+
+    ; index
+    xor rcx, rcx
+
+    .byte_loop:
+        ; index in the byte table. since each element in the table is
+        ; 64 bytes, the index should be multiplied by 64, which is 2**6
+        movzx rax, byte [byte_buffer + rcx]
+        shl rax, 6
+
+        ; copy from the table to the destination
+        vmovdqa64 zmm0, [table + rax]
+        vmovdqu64 [rdi], zmm0
+
+        ; the difference between the length of `<zero/>` and the length
+        ; of `<one/>` is exactly 1 byte, so the difference when writing
+        ; a full byte as "tag bits" is 8 * zero.len - popcnt
+        add rdi, 8 * zero.len
+        movzx rbx, byte [popcnt_buffer + rcx]
+        sub rdi, rbx
+
+        inc rcx
+        cmp rcx, r9
+        jne .byte_loop
+
+    add rsi, 64
+    cmp rdx, rsi
+    ja .64bytes_loop
+
 
     sub rdi, [output_mapped_ptr]
     mov [output_len], rdi
     call deinit
-
     jmp exit
 
 include 'common.asm'
