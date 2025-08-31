dc36c890334ae8e22b016a19d55b3586aac6167c
Netanel Friedman <netanel.friedman6@gmail.com>
2025-08-31 21:55:27 +0300

    Just use buffers

--- src/encode.asm
+++ src/encode.asm
@@ -2,88 +2,86 @@
 format ELF64 executable
 entry _start
 
-BUFFER_CAP equ (1024 * 1024)
+INPUT_BUFFER_CAP equ (128 * 1024)
+OUTPUT_BUFFER_CAP equ (INPUT_BUFFER_CAP * 56 + 8)
+
+SYS_read equ 0
+SYS_write equ 1
+
+STDIN_FD equ 0
+STDOUT_FD equ 1
 
 segment readable writeable
 align 64
-buffer: rb BUFFER_CAP
+input_buffer: rb INPUT_BUFFER_CAP
+align 64
+output_buffer: rb OUTPUT_BUFFER_CAP
 
 segment readable executable
 _start:
-    call init_input
-    call mmap_input
+    .read_loop:
+        ; read to the input buffer
+        mov rax, SYS_read
+        mov rdi, STDIN_FD
+        mov rsi, input_buffer
+        mov rdx, INPUT_BUFFER_CAP
+        syscall
 
-    ; calculate the maximum possible length for the output
-    mov rax, [input_len]  ; len
-    shl rax, 6            ; rax *= 64
-    mov [output_max_len], rax
+        ; exit if no more bytes left, or print error when error occures
+        cmp rax, 0
+        je exit
+        jl .error
 
-    call init_output
+        ; initialize pointers to the buffers
+        mov rsi, input_buffer
+        mov rdi, output_buffer
 
-    mov r10, buffer ; dst mapped addr
-    mov r13, [input_mapped_ptr]  ; src mapped addr
+        ; the amount of bytes read is the number of iterations
+        mov rcx, rax
 
-    mov rcx, [input_len]
+        .byte_loop:
+            movzx eax, byte [rsi]
 
-    ; pointer to the end of src
-    lea r12, [r13 + rcx]
+            ; each item in the table is 64 bytes in size (56 maximum string length + 8 bytes for alignment).
+            ; which is 2**6
+            shl eax, 6
 
-    ; TODO: instead of handling one byte try to handle multiple at once
-    .byte_loop:
-        movzx eax, byte [r13]
+            ; copy from the table to the destination
+            vmovdqa64 zmm0, [table + eax]
+            vmovdqu64 [rdi], zmm0
 
-        ; index in the byte table. since each element in the table is
-        ; 64 bytes, the index should be multiplied by 64, which is 2**6
-        shl ax, 6
+            ; advance by the actual string length: 56 - popcnt
+            popcnt rax, rax
+            add rdi, 56
+            sub rdi, rax
 
-        ; copy from the table to the destination
-        vmovdqa64 zmm3, [table + eax]
-        vmovdqu64 [r10], zmm3
+            ; next byte
+            inc rsi
+            loop .byte_loop
 
-        ; I'm not sure if this is the most efficient approach for the length
-        ; but right now this isn't the bottleneck.
-        popcnt rax, rax
-        add r10, 56
-        sub r10, rax
 
-        ; if the next chunk can fill the buffer, flush now.
-        cmp r10, buffer + BUFFER_CAP - 56
-        jb .dont_flush
+        ; flush the output buffer
+        lea rdx, [rdi - output_buffer] ; bytes written count
+        mov rax, SYS_write
+        mov rdi, STDOUT_FD
+        mov rsi, output_buffer
+        syscall
 
-        ; can be inlined but barely impacts performance.
-        call flush_buffer
-        .dont_flush:
+        jmp .read_loop
 
-        ; continue looping if the end of src ptr (r12) is
-        ; still bigger than the current src ptr (r13)
-        inc r13
-        cmp r12, r13
-        ja .byte_loop
+; won't fallthrough because of the `read` syscall
 
-    ; one last flush. if there's nothing to flush then
-    ; it's a small wasted syscall, but barely impacts performance.
-    call flush_buffer
+.error:
+    mov rsi, read_error_msg.text
+    mov rdx, read_error_msg.len
+    jmp error
 
-    call deinit_input
-    call truncate_output
-    call close_output
-    jmp exit
 
-close_output:
-    mov rax, 3
-    mov rdi, [output_fd]
-    syscall
-    ret
+segment readable
+read_error_msg:
+.text db "can't read file", 10
+.len = $ - .text
 
-flush_buffer:
-    mov rax, 1
-    mov rdi, [output_fd]
-    mov rsi, buffer
-    lea rdx, [r10 - buffer]
-    add [output_len], rdx
-    syscall
-    mov r10, buffer ; reset the buffer to the start
-    ret
 
 include 'common.asm'
 include 'table.asm'
